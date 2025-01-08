fe3f402df0913c631e943c681dbfbb1b7a448b2f
Louis ARNAULT <l.arnault@kami.fr>
2025-01-08 10:21:16 +0100

    Add environment file config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,13 @@
 <?php
 
 use Hydra\SupersetBundle\Tests\TestKernel;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__) . '/.env')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+}
 
 $kernel = new TestKernel('test', true);
 $kernel->boot();
